af816e5b9ddec00a393f73720fa7f5df9f549871
Akshayahire-talentica <akshay.ahire@talentica.com>
2025-10-10 05:16:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     # ---- Simulate random latency (response time > 1s about 25% of time) ----
-    if random.random() < 0.25:
-        delay = random.uniform(1.2, 2.5)  # delay between 1.2s and 2.5s
-        time.sleep(delay)
+    #if random.random() < 0.25:
+       # delay = random.uniform(1.2, 2.5)  # delay between 1.2s and 2.5s
+      #  time.sleep(delay)
 
     # ---- Simulate 5xx errors (20% of requests) ----
     if random.random() < 0.2:
