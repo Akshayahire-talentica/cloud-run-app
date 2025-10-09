b6eb46ab23b2a5f733352c984060b9ef78f20e25
Akshayahire-talentica <akshay.ahire@talentica.com>
2025-10-09 19:34:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,14 @@
 from flask import Flask
-import time
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, World from Cloud Run! deployed using GitHub Actions"
-
-# ⚡️ Latency testing endpoint
-# This route intentionally delays the response for testing Cloud Monitoring alerts.
-@app.route('/slow')
-def slow():
-    # Introduce artificial delay (in seconds)
-    delay_seconds = 5
-    start_time = time.time()
-
-    # Keep CPU busy + sleep for realistic delay
-    while time.time() - start_time < delay_seconds:
-        _ = [x**2 for x in range(10000)]  # small CPU load
-
-    return f"This /slow endpoint took about {delay_seconds} seconds to respond!"
+    # Simulate 20% of requests returning HTTP 500
+    if random.random() < 0.2:
+        return "Internal Server Error (Test)", 500
+    return "Hello, World from Cloud Run! deployed using GitHub Actions", 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
-
-
-
